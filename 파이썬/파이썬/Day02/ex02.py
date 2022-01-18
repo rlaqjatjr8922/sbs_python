36e668a093f07beeee6482c94ec07e8483b6b52b
rlaqjatjr8922 <gimbeomseognam5@gmail.com>
2022-01-18 15:13:00 +0900

    py1........3

---
+++ 파이썬/파이썬/Day02/ex02.py
@@ -0,0 +1,20 @@
+s = "안녕하새요"
+#문자열 선택연산자 : [index]
+
+print(s[1])
+print(s[2])
+print(s[3])
+print(s[4])
+print(s[0])
+
+#슬라이싱
+print(s[0:2])
+# 문자열의 길이 - len
+print("문자열의길이" + str(len(s)))
+#문자열+숫자+
+
+
+
+
+
+
---
+++ 파이썬/파이썬/Day02/ex05.py
@@ -0,0 +1,9 @@
+
+A = input("a :")
+b = input("b :")
+
+#input 문자열로변환
+A = int(A)
+B = int(B)
+
+print ("a+B = ", A+B)
