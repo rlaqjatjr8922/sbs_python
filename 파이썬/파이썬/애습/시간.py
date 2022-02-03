00559bc022cc3785c8fe469c993f85ca9b7d8b06
rlaqjatjr8922 <gimbeomseognam5@gmail.com>
2022-02-03 18:31:43 +0900

    s

--- 파이썬/파이썬/애습/시간.py
+++ 파이썬/파이썬/애습/시간.py
@@ -1,15 +1,24 @@
 
 import time as a 
+import os
+
+p = 'C:/Users/user/Desktop/시간/'
 
 
-
-while True:
 
     
-    path = '파이썬/파이썬/애습/'
-    file = open (path + '현재시간.txt','wt',encoding='UTF-8')
-    a.sleep(1)
+    
+while True:
+
     s = a.strftime('%y/%m/%d (%a) %H:%M:%S')
+    
+    file = open (p + '현재시간.txt','wt',encoding='UTF-8')
+    a.sleep(1)
+    
     file.write(str(s) )
-    print ('q')
+    
+    
+    if os.path.exists(file):
+        os.remove(file)
+    print('파일이 삭재됌.')
     
